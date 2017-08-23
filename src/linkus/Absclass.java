b620d7547549e72b993547f822b26c5034d177e2
mayurvk <mayurvk18@gmail.com>
2017-08-24 01:25:19 +0530

    Add All Files

---
+++ src/linkus/Absclass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkus;
+
+/**
+ *
+ * @author Akshay
+ */
+public abstract class Absclass {
+    public static String username,p_id;
+}
